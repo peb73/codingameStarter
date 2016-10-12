228bb8eeaec2f9d89a59d1cfe62390678ad3258e
BOITEAU <boiteaupe@gmail.com>
2016-10-12 23:51:00 +0200

    first commit

---
+++ src/main/java/com/codingame/starter/Player.java
@@ -0,0 +1,31 @@
+package com.codingame.starter;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Player {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+
+		// game loop
+		while (true) {
+			int x = in.nextInt(); // x position of your pod
+			int y = in.nextInt(); // y position of your pod
+			int nextCheckpointX = in.nextInt(); // x position of the next check
+												// point
+			int nextCheckpointY = in.nextInt(); // y position of the next check
+												// point
+
+			// Write an action using System.out.println()
+			// To debug: System.err.println("Debug messages...");
+
+			// Edit this line to output the target position
+			// and thrust (0 <= thrust <= 100)
+			// i.e.: "x y thrust"
+			System.out.println(nextCheckpointY + " " + nextCheckpointY + " 50");
+
+		}
+	}
+}}
